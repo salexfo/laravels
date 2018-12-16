dd19e988a9e0ec630ad30ec2f68afff4ca7f85b8
Alexander <alex@slokoto.com>
2018-12-16 12:56:32 +0200

    added clockwork

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 	Jenssegers\Mongodb\MongodbServiceProvider::class,
+	 Clockwork\Support\Laravel\ClockworkServiceProvider::class,
 
     ],
 
@@ -224,6 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
 
     ],
 
